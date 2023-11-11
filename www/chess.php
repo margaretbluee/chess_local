3875c28fb216f09397c7317b107e34819991601b
margaretbluee <margarita.iliopoulou@hotmail.com>
2023-11-11 21:09:11 +0200

    here!

--- www/chess.php
+++ www/chess.php
@@ -1,12 +1,13 @@
 <?php
+print_r("HERE!!!!!!");
 
 $method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-// $request = explode('/', trim($_SERVER['SCRIPT_NAME'],'/’));
+ $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+// $request = explode('/', trim($_SERVER['SCRIPT_NAME'],'/'));
 // Σε περίπτωση που τρέχουμε php –S
 $input = json_decode(file_get_contents('php://input'),true);
 
-print_r($_SERVER);
+print_r($request);
 exit;
 
 switch ($r=array_shift($request)) {
