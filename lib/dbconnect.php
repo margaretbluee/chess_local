6ee98ca95b827d9a89413d615e249cdb31f73038
margaretbluee <margarita.iliopoulou@hotmail.com>
2023-11-12 12:04:55 +0200

    db

---
+++ lib/dbconnect.php
@@ -0,0 +1,23 @@
+<?php
+$host='localhost';
+$db ='chess_local';
+require_once 'db_upass.php';
+
+//username kai password tou xristi root
+//tha mpei sto git ignore(db_upass)
+$user=$DB_USER;
+$pass=$DB_PASS;
+
+if(gethostname()=='users.iee.ihu.gr'){
+    $mysqli = new mysqli($host,$user,$pass,$db,null,'/home/student/it/2017/it174948/mysql/run/mysql.sock');
+
+} else {
+    $pass = null;
+    $mysqli = new mysqli($host,$user,$pass,$db);
+}
+
+if($mysqli->connect_errno){
+    echo "Failed to connect to MySQL: (".
+    $mysqli->connect_errno.") ".$mysqli->connect_error;
+
+} ?>
--- www/chess.php
+++ www/chess.php
@@ -1,5 +1,7 @@
 <?php
-print_r("HERE!!!!!!");
+//print_r("HERE!!!!!!");
+require_once "../lib/dbconnect.php";
+require_once "../lib/board.php";
 
 $method = $_SERVER['REQUEST_METHOD'];
  $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
@@ -7,17 +9,17 @@ $method = $_SERVER['REQUEST_METHOD'];
 // Σε περίπτωση που τρέχουμε php –S
 $input = json_decode(file_get_contents('php://input'),true);
 
-print_r($request);
-exit;
+//print_r($request);
+//exit;
 
 switch ($r=array_shift($request)) {
 case 'board' :
 switch ($b=array_shift($request)) {
-case '':
+case '':    
 case null: handle_board($method);break;
-case 'piece': handle_piece($method, $request[0],$request[1],$input);
+case 'piece':// handle_piece($method, $request[0],$request[1],$input);
 break;
-case 'player': handle_player($method, $request[0],$input);
+case 'player':// handle_player($method, $request[0],$input);
 break;
 default: header("HTTP/1.1 404 Not Found");
 break;
@@ -27,4 +29,18 @@ default:
 header("HTTP/1.1 404 Not Found");
 exit;
 }
+function handle_board($method){
+    if($method=='GET') {
+        show_board();
+            } else if ($method=='POST'){
+                reset_board();
+
+            }else{
+                header('HTTP/1.1405 Method Not Allowed');
+            }
+}
+
+
 ?>
+
+
