f11a8ccd3add2aed7850ede874e1d6d9367fad6d
margaretbluee <margarita.iliopoulou@hotmail.com>
2023-11-14 15:11:08 +0200

    draw

--- www/js/mychess.js
+++ www/js/mychess.js
@@ -1,19 +1,18 @@
 function draw_empty_board() {
  
-    var t='<table id="chess table">';
+	var t='<table id="chess_table">';
     for (var i=8;i>0; i--) {
     t += '<tr>';
    
     for (var j=1;j<9;j++) {
     
-    t += '<td class="chess square" id="square_'+j+' '+i+'">' + j +','+i+'</td>';
+        t += '<td class="chess_square" id="square_'+j+'_'+i+'">' + j +','+i+'</td>'; 
     }
     t+='</tr>';
-    }
-    
-    t+='</table>';
-    $('#chess_board').html(t);
+}
+t+='</table>';
 
+$('#chess_board').html(t);
     }
 
 $(draw_empty_board);
