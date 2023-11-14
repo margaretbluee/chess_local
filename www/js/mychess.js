7c27011afc51f376c0a316d05f889812aff01074
margaretbluee <margarita.iliopoulou@hotmail.com>
2023-11-14 15:22:22 +0200

    Update mychess.js

--- www/js/mychess.js
+++ www/js/mychess.js
@@ -1,18 +1,14 @@
 function draw_empty_board() {
- 
-	var t='<table id="chess_table">';
-    for(var i=8;i>0; i--) {
+    var t='<table id="chess_table">';
+    for(var i=8;i>0;i--) {
     t += '<tr>';
-   
     for(var j=1;j<9;j++) {
-    
-        t += '<td class="chess_square" id="square_'+j+'_'+i+'">' + j +','+i+'</td>'; 
+    t += '<td class="chess_square"
+    id="square_'+j+'_'+i+'">' + j +','+i+'</td>';
     }
     t+='</tr>';
-}
-t+='</table>';
-
-$('#chess_board').html(t);
     }
-
-$(draw_empty_board);
+    t+='</table>';
+    $('#chess_board').html(t);
+    }
+    
